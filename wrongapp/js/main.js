a97e7973ae72e8993d9eee9e0ff72013e07da778
Christopher Addington <caddington@fullsail.edu>
2011-12-07 19:38:48 -0500

    Set stylings for wrong app pages | Placed validation function in min.js for wrong app also

--- wrongapp/js/main.js
+++ wrongapp/js/main.js
@@ -3,13 +3,22 @@
 // Assignment: Project 1
 // MIU Term 1112
 
+var parseTeamForm = function(data){
+    console.log(data);
+    console.log(localStorage);
+};
+
 $(document).ready(function(){
     
-    var tmForm = $('#teamForm');
+    var tmform = $('#teamform');
     
-    tmForm.validate();
-
-
+    tmform.validate({
+        invalidHandler: function(form, validator){},
+        submitHandler: function(){
+            var data = tmform.serializeArray();
+            parseTeamForm(data);
+        }
+    });
 });
 
 /*
