052348b09f9d48d9cba71dd14dff91a2d87e87d1
Christopher Addington <caddington@fullsail.edu>
2011-12-08 20:06:45 -0500

    Inserted Input hint & prompt for wrong app | Created an alert for successful add to localStorage

--- goodapp/js/main.js
+++ goodapp/js/main.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
             var data = tmform.serializeArray();
             parseTeamForm(data);
             saveLocal();
-    
+            alert("Team saved!");
         }
     });
 });
